515cb41d03eb9204b07bc5d61b89eafe124f8942
Varuwolf <brunobazalvarez@gmail.com>
2022-03-16 17:10:20 +0100

    añadido el modo noche

--- scripts.js
+++ scripts.js
@@ -6,12 +6,12 @@ const rain = document.getElementById("lluvia");
 const cloud = document.getElementById("nublado");
 const clear = document.getElementById("sol");
 const snow = document.getElementById("nieve");
-const myP = document.createElement("p");
 const myTime = document.createElement("p");
+const myP = document.createElement("p");
 const thunderstorm = document.getElementById("tormenta");
 const mist = document.getElementById("neblina");
-let fecha = new Date();
-// const horario = fecha.get();
+const myBody = document.querySelector("body");
+
 function time(time) {
   let secs = Math.floor((time / 1000) % 60);
   let minutes = Math.floor((time / (1000 * 60)) % 60);
@@ -24,7 +24,7 @@ function time(time) {
 const succes = (pos) => {
   const lat = /* pos.coords.latitude; */ 41.9038123;
   const lon = /* pos.coords.longitude; */ -8.8746549;
-  console.log(lat, lon);
+  console.log(pos);
   const key = "5ac175d2902db4555e70111f79332e19";
   const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}`;
 
@@ -35,25 +35,37 @@ const succes = (pos) => {
     .then((data) => {
       console.log(data);
       const now = data.current.dt;
+      const sunset = data.current.sunset;
+      console.log(now);
+      if (now >= sunset) {
+        myBody.classList = "day";
+      } else {
+        myBody.classList = "night";
+      }
       for (const horas of data.hourly.slice(8, 9)) {
         let clima = horas.weather.map((el) => el.main);
+        console.log(horas.dt);
         myTime.textContent = `A las ${time(horas.dt - now)}`;
         myP.textContent = `Habrá una temperatura media de ${horas.temp}ºC`;
 
         if (clima == "Clouds") {
-          cloud.append(myTime);
+          cloud.prepend(myTime);
           cloud.append(myP);
           cloud.removeAttribute("hidden");
         } else if (clima == "Rain" || clima == "Drizzle") {
+          rain.prepend(myTime);
           rain.append(myP);
           rain.removeAttribute("hidden");
         } else if (clima == "Clear") {
+          clear.prepend(myTime);
           clear.append(myP);
           clear.removeAttribute("hidden");
         } else if (clima == "Snow") {
+          snow.prepend(myTime);
           snow.append(myP);
           snow.removeAttribute("hidden");
         } else if (clima == "Thunderstorm") {
+          thunderstorm.prepend(myTime);
           thunderstorm.append(myP);
           thunderstorm.removeAttribute("hidden");
         } else if (
@@ -67,6 +79,7 @@ const succes = (pos) => {
           clima == "Squall" ||
           clima == "Tornado"
         ) {
+          mist.prepend(myTime);
           mist.append(myP);
           mist.removeAttribute("hidden");
         }
@@ -86,7 +99,6 @@ const options = {
 const handleClickButton = () => {
   const geolocation = navigator.geolocation;
   geolocation.getCurrentPosition(succes, error, options);
-  console.log(geolocation);
   actGps.classList.remove("activo");
 };
 
