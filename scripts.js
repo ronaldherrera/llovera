686c84f7aa0a86add4427de46bae72f9082b0f85
Varuwolf <brunobazalvarez@gmail.com>
2022-03-18 17:50:58 +0100

    Añadidos clima por horas y tocando css

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const myButton = document.querySelector("button");
+const myButton = document.querySelector("#boton");
 const actGps = document.getElementById("gps");
 const rain = document.getElementById("lluvia");
 const cloud = document.getElementById("nublado");
@@ -15,31 +15,32 @@ const myBody = document.querySelector("body");
 const myTemp = document.createElement("p");
 const myUl = document.querySelector("ul");
 const imgNube = (document.createElement("img").src =
-  "./Recursos/G.3.nublado-animacion.gif");
+  "./Recursos/nublado-animacion.gif");
 const imgLuna = (document.createElement("img").src =
-  "./Recursos/G.1.1luna-animacion.gif");
+  "./Recursos/luna-animacion.gif");
 const imgSol = (document.createElement("img").src =
-  "./Recursos/G.1.sol-animacion.gif");
+  "./Recursos/sol-animacion.gif");
 const imgPocasNubes = (document.createElement("img").src =
-  "./Recursos/G.2.pocas-nubes-animacion.gif");
+  "./Recursos/pocas-nubes-animacion.gif");
 const imgLluvia = (document.createElement("img").src =
-  "./Recursos/G.4.lluvia-animacion.gif");
+  "./Recursos/lluvia-animacion.gif");
 const imgTormenta = (document.createElement("img").src =
-  "./Recursos/G.6.tormenta-animacion.gif");
+  "./Recursos/tormenta-animacion.gif");
 const imgNieve = (document.createElement("img").src =
-  "./Recursos/G.7.nieve-animacion.gif");
+  "./Recursos/nieve-animacion.gif");
 const imgNiebla = (document.createElement("img").src =
-  "./Recursos/G.8.niebla-animacion.gif");
-
+  "./Recursos/niebla-animacion.gif");
 const sol = document.getElementById("sol");
 const luna = document.getElementById("luna");
 const now = new Date().toLocaleString("es-ES", {
   timeStyle: "short",
   dateStyle: "long",
 });
+const myPosition = document.createElement("p");
+
 const succes = async (pos) => {
-  const lat = await /* pos.coords.latitude */ 41.9025;
-  const lon = await /* pos.coords.longitude */ -8.87367;
+  const lat = await pos.coords.latitude;
+  const lon = await pos.coords.longitude;
   fetchData(lat, lon);
   currentWeatherData(lat, lon);
 };
@@ -55,43 +56,48 @@ async function fetchData(lat, lon) {
 
 const currentWeatherData = async (lat, lon) => {
   const key = "5ac175d2902db4555e70111f79332e19";
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${key}`;
   const resp = await fetch(url);
   const data = await resp.json();
   myP.textContent = ` ${data.name}`;
   let clima = data.weather;
-  console.log(data);
+  let temp = data.main.temp;
+  let position = data.name;
   for (const datos of clima) {
-    myTemp.textContent = `${datos.temp}`;
+    myTemp.textContent = `${Math.round(temp)}ºC`;
     myTime.textContent = `${now}`;
+    myPosition.textContent = `${position}`;
     const hora = new Date(data.dt * 1000).getHours();
     if (hora > 7 && hora < 20) {
-      sol.removeAttribute("hidden");
+      sol.style.display = "grid";
+      luna.style.display = "none";
     } else {
-      luna.removeAttribute("hidden");
+      luna.style.display = "grid";
+      sol.style.display = "none";
     }
     switch (datos.main) {
       case "Clouds":
-        cloud.prepend(myTime),
-          cloud.append(myP),
-          cloud.removeAttribute("hidden");
+        cloud.prepend(myTime), cloud.append(myTemp), cloud.append(myPosition);
+        cloud.style.display = "grid";
         break;
       case "Rain":
       case "Drizzle":
-        rain.prepend(myTime), rain.append(myP), rain.removeAttribute("hidden");
+        rain.prepend(myTime), rain.append(myTemp), rain.append(myPosition);
+        rain.style.display = "grid";
         break;
       case "Clear":
-        clear.prepend(myTime),
-          clear.append(myP),
-          clear.removeAttribute("hidden");
+        clear.prepend(myTime), clear.append(myTemp), clear.append(myPosition);
+        clear.style.display = "grid";
         break;
       case "Snow":
-        snow.prepend(myTime), snow.append(myP), snow.removeAttribute("hidden");
+        snow.prepend(myTime), snow.append(myTemp), snow.append(myPosition);
+        snow.style.display = "grid";
         break;
       case "Thunderstorm":
         thunderstorm.prepend(myTime),
-          thunderstorm.append(myP),
-          thunderstorm.removeAttribute("hidden");
+          thunderstorm.append(myTemp),
+          thunderstorm.append(myPosition);
+        thunderstorm.style.display = "grid";
         break;
       case "Mist":
       case "Smoke":
@@ -103,8 +109,9 @@ const currentWeatherData = async (lat, lon) => {
       case "Squall":
       case "Tornado":
         mist.prepend(myTime);
-        mist.append(myP);
-        mist.removeAttribute("hidden");
+        mist.append(myTemp);
+        mist.append(myPosition);
+        mist.style.display = "grid";
         break;
     }
   }
@@ -113,15 +120,15 @@ const weatherData = async (data) => {
   // console.log(data);
 
   for (const horas of [data.hourly.slice(1, 9)]) {
-    //Se coloca la hora que horá y la temperaturá que habrá a esa hora
+    //Se coloca la hora que horá y la temperatura que habrá a esa hora
     const newWeathers = horas.map((clima) => {
       return {
         hora: new Date(clima.dt * 1000).toLocaleString("es-ES", {
           hour: "2-digit",
           minute: "2-digit",
         }),
-        temp: clima.temp,
-        meteo: clima.weather,
+        temp: Math.round(clima.temp),
+        meteo: clima.weather.map((el) => el.main).join(),
       };
     });
     console.log(newWeathers);
@@ -129,49 +136,61 @@ const weatherData = async (data) => {
   }
 };
 
-const listWeather = async (newWeather) => {
-  let meteo = newWeather.filter((el) => el.meteo.main);
-  console.log(meteo);
-  const climaFunction = (meteo) => {
-    for (let datos of meteo) {
-      switch (datos) {
-        case "Clouds":
-          imgNube;
-          break;
-        case "Rain":
-        case "Drizzle":
-          imgLluvia;
-          break;
-        case "Clear":
-          imgSol, imgLuna;
-          break;
-        case "Snow":
-          imgNieve;
-          break;
-        case "Thunderstorm":
-          imgTormenta;
-          break;
-        case "Mist":
-        case "Smoke":
-        case "Haze":
-        case "Dust":
-        case "Fog":
-        case "Sand":
-        case "Ash":
-        case "Squall":
-        case "Tornado":
-          imgNiebla;
-          break;
-      }
+const listWeather = async (newWeathers) => {
+  console.log(newWeathers);
+  const arrayHTMLweather = newWeathers.map((clima) => {
+    switch (clima.meteo) {
+      case "Clouds":
+        return ` <li>
+    <p>${clima.hora}</p>
+    <img src="${imgNube}">
+    <p>${clima.temp}ºC</p>`;
+
+      case "Rain":
+      case "Drizzle":
+        return ` <li>
+    <p>${clima.hora}</p>
+    <img src="${imgLluvia}">
+    <p>${clima.temp}ºC</p>`;
+
+      case "Clear":
+        if (clima.hora > "07:00" && clima.hora < "20:00") {
+          return ` <li>
+    <p>${clima.hora}</p>
+    <img src="${imgSol}">
+    <p>${clima.temp}ºC</p>`;
+        } else {
+          return ` <li>
+      <p>${clima.hora}</p>
+      <img src="${imgLuna}">
+      <p>${clima.temp}ºC</p>`;
+        }
+      case "Snow":
+        return ` <li>
+    <p>${clima.hora}</p>
+    <img src="${imgNieve}">
+    <p>${clima.temp}ºC</p> `;
+
+      case "Thunderstorm":
+        return ` <li>
+    <p>${clima.hora}</p>
+    <img src="${imgTormenta}">
+    <p>${clima.temp}ºC</p> `;
+
+      case "Mist":
+      case "Smoke":
+      case "Haze":
+      case "Dust":
+      case "Fog":
+      case "Sand":
+      case "Ash":
+      case "Squall":
+      case "Tornado":
+        return ` <li>
+    <p>${clima.hora}</p>
+    <img src="${imgNiebla}">
+    <p>${clima.temp}ºC</p> `;
     }
-  };
-  const arrayHTMLweather = newWeather.map((clima) => {
-    return `
-    <li>
-    <h2>${clima.hora}</h2>
-    <img src="${climaFunction(meteo)}">
-    <p>${clima.meteo.map((el) => el.main)}</p>
-    <p>${clima.temp}</p>`;
   });
   myUl.innerHTML = `<ul>${arrayHTMLweather.join("")}</ul>`;
 };
@@ -185,7 +204,6 @@ const backgroundDiaNoche = async (data) => {
     myBody.classList.remove("dia");
     myBody.classList.add("noche");
   }
-
 };
 
 const error = (error) => {
@@ -200,10 +218,8 @@ const options = {
 const handleClickButton = () => {
   const geolocation = navigator.geolocation;
   geolocation.getCurrentPosition(succes, error, options);
-
   carrusel.removeAttribute("hidden");
   actGps.classList.remove("activo");
-
 };
 
 myButton.addEventListener("click", handleClickButton);
