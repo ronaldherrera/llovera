0c6c64f558f7bc87425c3d84e7b60e3c82fd2e85
Varuwolf <brunobazalvarez@gmail.com>
2022-03-21 16:13:42 +0100

    Añadido localstorage de las coordenadas

--- JS/scripts.js
+++ JS/scripts.js
@@ -4,16 +4,34 @@ import {
   weatherData,
   backgroundDiaNoche,
 } from "./weatherData.js";
+let localStorageLat;
+let localStorageLon;
+const localStorageCoords = window.localStorage.getItem("coords");
 const myButton = document.querySelector("button");
 const actGps = document.getElementById("gps");
 
 const succes = async (pos) => {
   const lat = await pos.coords.latitude;
   const lon = await pos.coords.longitude;
+  const coordsOBJ = { lat, lon };
+  localStorage.setItem("permission", "ok");
+  localStorageLat = localStorage.setItem("lat", lat);
+  localStorageLon = localStorage.setItem("lon", lon);
   fetchData(lat, lon);
   currentWeatherData(lat, lon);
 };
-
+async function loadCoord() {
+  const loadedCoords = localStorage.getItem(localStorageCoords);
+  if (loadedCoords === null) {
+    handleClickButton();
+  } else {
+    const lat = await localStorageLat;
+    const lon = await localStorageLon;
+    console.log(lat, lon);
+    fetchData(lat, lon);
+    currentWeatherData(lat, lon);
+  }
+}
 async function fetchData(lat, lon) {
   const key = "5ffc44f30e5179f5e6420ea84b83cb9a";
   const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&exclude=minutely,daily&appid=${key}`;
@@ -39,3 +57,7 @@ const handleClickButton = () => {
 };
 
 myButton.addEventListener("click", handleClickButton);
+function init() {
+  loadCoord();
+}
+init();
